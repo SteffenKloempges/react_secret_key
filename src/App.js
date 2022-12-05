52336a0a1e97a301716fe98e100ca0d45c65af6c
SteffenKloempges <steffen.kloempges@gmail.com>
2022-12-05 08:57:31 +0100

    add key

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import { useState, useEffect } from 'react';
+import apiKey from './apiKey.json'
 
 function App() {
+  console.log(apiKey);
+  // const [data, setData] = useState()
+
+  // useEffect(() => {
+  //   fetch(`https://api.unsplash.com/photos/?client_id=${apiKey[0].key}`)
+  //     .then(response => response.json())
+  //     .then(json => setData(json))
+  // }, [])
+
+  // console.log(data)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hallöchen ihr lieben</h1>
       </header>
     </div>
   );
